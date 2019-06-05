bced554202368360876a9db552e90f816b534dd4
Nor014 <44439305+Nor014@users.noreply.github.com>
2019-06-05 21:22:07 +0700

    Add files via upload

---
+++ hj-30-Nor014-master/js/draw.js
@@ -0,0 +1,88 @@
+
+const drawTool = document.querySelector('.draw'),
+  drawColors = document.querySelectorAll('.draw-tools input')
+
+
+let mousedown,
+  moveToX,
+  moveToY,
+  drawColor,
+  canvas,
+  ctx
+
+
+function beginDraw() {
+  if (drawTool.style.display === 'inline-block') {
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.beginPath()
+    mousedown = true;
+    moveToX = event.offsetX;
+    moveToY = event.offsetY;
+    ctx.lineWidth = 4;
+
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+
+    drawColor = Array.from(drawColors).find(color => color.checked).value
+
+    if (drawColor === 'red') ctx.strokeStyle = '#ea5d56'
+    if (drawColor === 'yellow') ctx.strokeStyle = '#f3d135'
+    if (drawColor === 'green') ctx.strokeStyle = '#6cbe47'
+    if (drawColor === 'blue') ctx.strokeStyle = '#53a7f5'
+    if (drawColor === 'purple') ctx.strokeStyle = '#b36ade'
+  }
+}
+
+
+function draw() {
+  if (mousedown && drawTool.style.display === 'inline-block') {
+    ctx.beginPath()
+
+    ctx.moveTo(moveToX, moveToY)
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    ctx.closePath();
+
+    moveToX = event.offsetX;
+    moveToY = event.offsetY;
+
+    // canvasThrottle(sendBlob, 100)
+
+    // setTimeout(function() {
+    //   sendBlob() 
+    // }, 1000)
+
+  }
+}
+
+function stopDraw() {
+  if (drawTool.style.display === 'inline-block') {
+    sendBlob()
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    mousedown = false;
+    moveToX = null;
+    moveToY = null;
+  }
+}
+
+function canvasThrottle(callback, delay) {
+
+  let isWaiting = false;
+
+  return function () {
+    if (!isWaiting) {
+      callback.apply(this, arguments);
+      isWaiting = true;
+      setTimeout(() => {
+        isWaiting = false;
+      }, delay)
+    }
+  }
+}
+
+function sendBlob() {
+  canvas.toBlob(function (blob) {
+    connection.send(blob)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  })
+}
